27ed081d9eb2692c600ba57e3d3036c499816253
petko <p.todorov1015@gmail.com>
2023-01-19 15:11:49 +0200

    new

--- Advanced/Exercises/05 Stacks, Queues, Tuples and Sets - Exercise/01_numbers.py
+++ Advanced/Exercises/05 Stacks, Queues, Tuples and Sets - Exercise/01_numbers.py
@@ -2,10 +2,10 @@ one = set([int(x) for x in input().split()])
 two = set([int(x) for x in input().split()])
 
 commands = {
-    "Add First": lambda x: [one.add(element) for element in x],
-    "Add Second": lambda x: [two.add(element) for element in x],
-    "Remove First": lambda x: [one.discard(element) for element in x],
-    "Remove Second": lambda x: [two.discard(element) for element in x],
+    "Add First": lambda x: {one.add(element) for element in x},
+    "Add Second": lambda x: {two.add(element) for element in x},
+    "Remove First": lambda x: {one.discard(element) for element in x},
+    "Remove Second": lambda x: {two.discard(element) for element in x},
     "Check Subset": lambda: print(True) if two.issubset(one) else print(False)
 }
 
