f00e6e8e4a420aa28f97775aeba9c6cf428ecb04
petko <p.todorov1015@gmail.com>
2023-02-25 16:14:08 +0200

    new

---
+++ OOP/03 Classes and Objects - Lab/05_smartphone.py
@@ -0,0 +1,25 @@
+class Smartphone:
+    def __init__(self, memory: int):
+        self.memory = memory
+        self.apps = []
+        self.is_on = False
+
+    def power(self):
+        if self.is_on:
+            self.is_on = False
+        else:
+            self.is_on = True
+
+    def install(self, app, app_memory):
+        if self.memory >= app_memory:
+            if self.is_on:
+                self.apps.append(app)
+                self.memory -= app_memory
+                return f"Installing {app}"
+            else:
+                return f"Turn on your phone to install {app}"
+
+        return f"Not enough memory to install {app}"
+
+    def status(self):
+        return f"Total apps: {len(self.apps)}. Memory left: {self.memory}"
