7e53bae6d0a23a3cdf9567c10c915b379e7d114e
petko <p.todorov1015@gmail.com>
2023-03-03 12:05:57 +0200

    new

---
+++ OOP/07 Encapsulation - Lab/03_profile.py
@@ -0,0 +1,28 @@
+class Profile:
+    def __init__(self, username: str, password: str):
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self.__username
+
+    @username.setter
+    def username(self, value):
+        if not 5 < len(value) < 15:
+            raise ValueError("The username must be between 5 and 15 characters.")
+        self.__username = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if len(value) < 8 or not any(x.isupper() for x in value) or \
+                not any(x.isdigit() for x in value):
+            raise ValueError("The password must be 8 or more characters with at least 1 digit and 1 uppercase letter.")
+        self.__password = value
+
+    def __str__(self):
+        return f'You have a profile with username: "{self.username}" and password: {"*" * len(self.__username)}'
