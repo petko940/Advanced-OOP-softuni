f51c556ee5a3ad688fef58f5fefcd5fe4f227e30
petko <p.todorov1015@gmail.com>
2023-03-07 13:20:37 +0200

    new

---
+++ OOP/08 Encapsulation - Exercise/03_football_team_generator/project/team.py
@@ -0,0 +1,25 @@
+from project.player import Player
+
+
+class Team:
+    def __init__(self, name: str, rating: int):
+        self.__name = name
+        self.__rating = rating
+        self.__players: list[object] = []
+
+    def add_player(self, player: Player):
+        if player in self.__players:
+            return f"Player {player.name} has already joined"
+
+        self.__players.append(player)
+        return f"Player {player.name} joined team {self.__name}"
+
+    def remove_player(self, player_name: str):
+        for p in self.__players:
+            if p.name == player_name:
+                self.__players.remove(p)
+                return p
+        return f"Player {player_name} not found"
+
+
+
