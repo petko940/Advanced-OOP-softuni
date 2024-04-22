3686f899eafed232b876b6f833e5eab487340fe9
Petko Todorov <p.todorov1015@gmail.com>
2024-04-22 12:54:52 +0300

    updated

--- Advanced/Exams/12 April 2023/01_rubber_duck_debuggers.py
+++ Advanced/Exams/12 April 2023/01_rubber_duck_debuggers.py
@@ -1,34 +1,71 @@
 from collections import deque
 
-programmers_time = deque([int(x) for x in input().split()])
-number_of_tasks = [int(x) for x in input().split()]
+programmer_time_to_complete = deque(int(x) for x in input().split())
+tasks = deque(int(x) for x in input().split())
 
-rubber_ducky_typ = {
+rubber_ducky_types = {
     'Darth Vader Ducky': range(0, 61),
     'Thor Ducky': range(61, 121),
     'Big Blue Rubber Ducky': range(121, 181),
     'Small Yellow Rubber Ducky': range(181, 241),
 }
 
-output = {'Darth Vader Ducky': 0,
-          'Thor Ducky': 0,
-          'Big Blue Rubber Ducky': 0,
-          'Small Yellow Rubber Ducky': 0}
+result = {
+    'Darth Vader Ducky': 0,
+    'Thor Ducky': 0,
+    'Big Blue Rubber Ducky': 0,
+    'Small Yellow Rubber Ducky': 0
+}
 
-while programmers_time and number_of_tasks:
-    first = programmers_time.popleft()
-    last = number_of_tasks.pop()
+while programmer_time_to_complete and tasks:
+    first_programmer_time = programmer_time_to_complete.popleft()
+    last_task = tasks.pop()
 
-    result = first * last
-    for key, value in rubber_ducky_typ.items():
-        if result in value:
-            output[key] = output.get(key, 0) + 1
+    multiply = first_programmer_time * last_task
+
+    for key, value in rubber_ducky_types.items():
+        if multiply in value:
+            result[key] += 1
             break
     else:
-        programmers_time.append(first)
-        number_of_tasks.append(last - 2)
+        programmer_time_to_complete.append(first_programmer_time)
+        tasks.append(last_task - 2)
 
-print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+print("Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+[print(f"{key}: {value}") for key, value in result.items()]
 
-for k, v in output.items():
-    print(f"{k}: {v}")
+
+# from collections import deque
+#
+# programmers_time = deque([int(x) for x in input().split()])
+# number_of_tasks = [int(x) for x in input().split()]
+#
+# rubber_ducky_typ = {
+#     'Darth Vader Ducky': range(0, 61),
+#     'Thor Ducky': range(61, 121),
+#     'Big Blue Rubber Ducky': range(121, 181),
+#     'Small Yellow Rubber Ducky': range(181, 241),
+# }
+#
+# output = {'Darth Vader Ducky': 0,
+#           'Thor Ducky': 0,
+#           'Big Blue Rubber Ducky': 0,
+#           'Small Yellow Rubber Ducky': 0}
+#
+# while programmers_time and number_of_tasks:
+#     first = programmers_time.popleft()
+#     last = number_of_tasks.pop()
+#
+#     result = first * last
+#     for key, value in rubber_ducky_typ.items():
+#         if result in value:
+#             output[key] = output.get(key, 0) + 1
+#             break
+#     else:
+#         programmers_time.append(first)
+#         number_of_tasks.append(last - 2)
+#
+# print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+#
+# for k, v in output.items():
+#     print(f"{k}: {v}")
